2e8e677d26a2c620da74e311113925e9c95b2264
pascalhow <pascal.how@mydrivesolutions.com>
2016-12-01 19:22:51 +0000

    Initial commit

---
+++ src/com/pascalhow/screenGrab/Main.java
@@ -0,0 +1,9 @@
+package com.pascalhow.screenGrab;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, World!");
+    }
+}
